c27f7d937bf6dcbe1e77724be1fe9600eb7e7b06
osipovea <osipovea28.09@gmail.com>
2024-11-07 21:02:50 +0300

    1 задача

--- Lists/01.py
+++ Lists/01.py
@@ -1,11 +1,7 @@
-n = int(input("Введите кол-во учеников"))
-students = []
+n = int(input("Введите кол-во покупок :"))
+products = []
 for i in range(n):
-  student = input().split()
-  students.append((student[0], int(student[1])))
-for student in students:
-  print(student[0], student[1])
-print()
-for student in students:
-  if student[1] >= 4:
-    print(student[0], student[1])
+  product = input("Введите название товара: ")
+  products.append(product)
+for product in products:
+  print(product)
