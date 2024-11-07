772505c15fff336a00e111447e534effef4273a5
osipovea <osipovea28.09@gmail.com>
2024-11-07 14:43:46 +0300

    Задача 2

---
+++ Conditional operator/03.py
@@ -0,0 +1,7 @@
+a = input("Введи строку 1: ")
+b = input("Введи строку 2: ")
+c = input("Введи строку 3: ")
+if a.lower() == "раз" and b.lower() == "два" and c.lower() == "три":
+  print("ГОРИ")
+else:
+  print("НЕ ГОРИ")
