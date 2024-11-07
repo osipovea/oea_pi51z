1f4baf2ac1f36a482a835259d9131f07f4910977
osipovea <osipovea28.09@gmail.com>
2024-11-07 14:09:17 +0300

    Task13

--- Tasks/13.py
+++ Tasks/13.py
@@ -1,1 +1,4 @@
+#выведет ответ как описание для ввода
 input(planet)
+#можно повторно задать значение переменной warning, но тогда получим warn "Redeclared 'warning' defined above without usage"
+warning=planet
